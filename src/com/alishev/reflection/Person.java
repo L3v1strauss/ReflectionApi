a191bdeb8001bf1c147e945134b313dab47a9c73
L3v1strauss <L3v1s@mail.ru>
2021-02-06 18:01:00 +0300

    Reflection Api 1 example

---
+++ src/com/alishev/reflection/Person.java
@@ -0,0 +1,35 @@
+package com.alishev.reflection;
+
+@Author(name = "Mikhail", dateOfCreation = 6)
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Person with id " + id + " and name " + name + " say Hello!");
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
